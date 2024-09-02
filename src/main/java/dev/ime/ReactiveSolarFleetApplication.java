b84ca08278c2b21bfd9e97ee7bc9235fd9b6a7db
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-09-02 07:51:21 +0200

    feat: initial commit

---
+++ src/main/java/dev/ime/ReactiveSolarFleetApplication.java
@@ -0,0 +1,13 @@
+package dev.ime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReactiveSolarFleetApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReactiveSolarFleetApplication.class, args);
+	}
+
+}
