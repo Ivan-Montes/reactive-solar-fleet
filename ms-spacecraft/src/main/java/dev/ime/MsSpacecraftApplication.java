031da27564d78391c0be79e0553320e8484ec788
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-09-03 07:36:42 +0200

    feat: ms-spacecraft init commit

---
+++ ms-spacecraft/src/main/java/dev/ime/MsSpacecraftApplication.java
@@ -0,0 +1,13 @@
+package dev.ime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsSpacecraftApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsSpacecraftApplication.class, args);
+	}
+
+}
