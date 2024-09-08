c3c1dccf2179f337aba80ecf88c09d24d5b644d5
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-09-08 09:31:53 +0200

    feat: add redis config

---
+++ ms-position/src/main/java/dev/ime/config/RedisConfig.java
@@ -0,0 +1,30 @@
+package dev.ime.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import dev.ime.infrastructure.entity.CrewMemberRedisEntity;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    ReactiveRedisTemplate<String, CrewMemberRedisEntity> reactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+        Jackson2JsonRedisSerializer<CrewMemberRedisEntity> serializer = new Jackson2JsonRedisSerializer<>(CrewMemberRedisEntity.class);
+        
+        RedisSerializationContext.RedisSerializationContextBuilder<String, CrewMemberRedisEntity> builder =
+            RedisSerializationContext.newSerializationContext( RedisSerializer.string() );
+        
+        RedisSerializationContext<String, CrewMemberRedisEntity> context = builder
+            .value(serializer)
+            .build();
+        
+        return new ReactiveRedisTemplate<>(factory, context);
+        
+    }
+}
