e3440908eccc53492c19505f8297f6c205fe7d2e
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-09-12 07:51:31 +0200

    feat: add logger classes

---
+++ ms-shipclass/src/main/java/dev/ime/config/LoggerUtil.java
@@ -0,0 +1,34 @@
+package dev.ime.config;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class LoggerUtil {
+
+	private final Logger logger;
+
+	public LoggerUtil(Logger logger) {
+		super();
+		this.logger = logger;
+	}	
+	
+	public void logSevereAction(String msg) {
+		
+		String logMessage = String.format(GlobalConstants.MSG_PATTERN_SEVERE, msg);
+		
+		logger.log(Level.SEVERE, logMessage);
+		
+	} 
+
+	public void logInfoAction(String className, String methodName, String msg) {
+		
+		String logMessage = String.format(GlobalConstants.MSG_PATTERN_INFO, className, methodName, msg);
+		
+		logger.log(Level.INFO, logMessage);
+		
+	}
+		
+}
