37009ba8657f206476364a7f45d4941963d51281
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-09-16 07:37:50 +0200

    feat: add entity redis spacecraft

---
+++ ms-shipclass/src/main/java/dev/ime/infrastructure/entity/SpacecraftRedisEntity.java
@@ -0,0 +1,23 @@
+package dev.ime.infrastructure.entity;
+
+import java.util.UUID;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.redis.core.RedisHash;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@RedisHash
+@NoArgsConstructor
+@AllArgsConstructor
+@Getter
+public class SpacecraftRedisEntity {
+	
+	@Id
+	private UUID spacecraftId;
+	
+	private UUID shipclassId;
+	
+}
