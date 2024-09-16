f7eecb0148829a9e572dabb664e9188119026fc9
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-09-16 07:41:45 +0200

    feat: add Shipclass repo adapter

---
+++ ms-shipclass/src/main/java/dev/ime/infrastructure/adapter/ShipclassReadRepositoryAdapter.java
@@ -0,0 +1,50 @@
+package dev.ime.infrastructure.adapter;
+
+import java.util.UUID;
+
+import org.springframework.stereotype.Repository;
+
+import dev.ime.config.ShipclassMapper;
+import dev.ime.domain.model.Shipclass;
+import dev.ime.domain.port.outbound.ReadRepositoryPort;
+import dev.ime.infrastructure.repository.ShipclassReadRepository;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+@Repository
+public class ShipclassReadRepositoryAdapter implements ReadRepositoryPort<Shipclass> {
+
+	private final ShipclassReadRepository shipclassReadRepository;
+	private final ShipclassMapper mapper;
+	
+	public ShipclassReadRepositoryAdapter(ShipclassReadRepository shipclassReadRepository, ShipclassMapper mapper) {
+		super();
+		this.shipclassReadRepository = shipclassReadRepository;
+		this.mapper = mapper;
+	}
+
+	@Override
+	public Flux<Shipclass> findAll() {
+
+		return shipclassReadRepository
+		.findAll()
+		.map(mapper::fromJpaToDomain);
+	}
+
+	@Override
+	public Mono<Shipclass> findById(UUID id) {
+		
+		return shipclassReadRepository
+				.findById(id)
+				.map(mapper::fromJpaToDomain);	
+	}
+
+	@Override
+	public Mono<Shipclass> findByName(String name) {
+		
+		return shipclassReadRepository
+				.findByShipclassName(name)
+				.map(mapper::fromJpaToDomain);
+	}
+
+}
