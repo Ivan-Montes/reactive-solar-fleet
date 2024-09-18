fe9a3ea592284350bea5f73591afc2a8e988ced9
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-09-18 07:42:10 +0200

    feat: add ms-crewmember initial commit

---
+++ ms-crewmember/src/main/java/dev/ime/MsCrewmemberApplication.java
@@ -0,0 +1,13 @@
+package dev.ime;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsCrewmemberApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsCrewmemberApplication.class, args);
+	}
+
+}
