33791dff0b84c9d3c9b9f343ccb1efce184234e8
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-09-18 07:51:42 +0200

    refactor: extract operations into methods

--- ms-shipclass/src/main/java/dev/ime/infrastructure/adapter/KafkaShipclassSubscriberAdapter.java
+++ ms-shipclass/src/main/java/dev/ime/infrastructure/adapter/KafkaShipclassSubscriberAdapter.java
@@ -1,13 +1,15 @@
 package dev.ime.infrastructure.adapter;
 
 import java.util.Map;
-import java.util.function.Consumer;
+import java.util.function.Function;
 
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.kafka.annotation.KafkaListener;
 import org.springframework.stereotype.Component;
 
+import dev.ime.application.exception.EmptyResponseException;
+import dev.ime.application.exception.ValidationException;
 import dev.ime.config.GlobalConstants;
 import dev.ime.config.LoggerUtil;
 import dev.ime.domain.event.Event;
@@ -20,7 +22,7 @@ import reactor.core.publisher.Mono;
 public class KafkaShipclassSubscriberAdapter implements SubscriberPort{
 
 	private final LoggerUtil loggerUtil;
-    private final Map<String, Consumer<Event>> actionsMap;  
+    private final Map<String, Function<Event, Mono<?>>> actionsMap;  
     private final BaseProjectorPort baseProjectorPort;
     private final ExtendedProjectorPort extendedProjectorPort;
 
@@ -33,7 +35,7 @@ public class KafkaShipclassSubscriberAdapter implements SubscriberPort{
 		this.actionsMap = initializeActionsMap();
 	}
 
-	private Map<String, Consumer<Event>> initializeActionsMap() {
+	private Map<String, Function<Event, Mono<?>>> initializeActionsMap() {
 		
 		return Map.of(
                 GlobalConstants.SHIPCLASS_CREATED, baseProjectorPort::create,
@@ -45,30 +47,52 @@ public class KafkaShipclassSubscriberAdapter implements SubscriberPort{
 	
 	@Override	
 	@KafkaListener(topics = {GlobalConstants.SHIPCLASS_CREATED, GlobalConstants.SHIPCLASS_UPDATED, GlobalConstants.SHIPCLASS_DELETED}, groupId = "msshipclass-consumer-shipclass")
-	public void onMessage(ConsumerRecord<String, Event> consumerRecord) {
+	public Mono<Void> onMessage(ConsumerRecord<String, Event> consumerRecord) {
 		
-		Mono.justOrEmpty(consumerRecord)
-		.doOnNext( recordObj -> loggerUtil.logInfoAction(this.getClass().getSimpleName(), recordObj.getClass().getSimpleName(), recordObj.toString()))
-		.map(ConsumerRecord::value)
-		.flatMap( event -> processEvent(consumerRecord.topic(), event) )
-		.onErrorResume( e -> Mono.fromRunnable(()-> loggerUtil.logSevereAction(e.getMessage()) ))
-		.subscribe();	
+		return Mono.justOrEmpty(consumerRecord)
+				.switchIfEmpty(Mono.error(new EmptyResponseException(Map.of(this.getClass().getSimpleName(), GlobalConstants.EX_EMPTYRESPONSE_DESC))))
+				.transform(this::logFlowStep)
+				.flatMap(this::validateTopic)
+				.flatMap(this::validateValue)
+			    .transform(this::logFlowStep)
+				.flatMap(this::processEvent)
+				.onErrorResume( error -> Mono.fromRunnable(()-> loggerUtil.logSevereAction( this.getClass().getSimpleName() + "]:[" +  GlobalConstants.MSG_FLOW_ERROR + "]:[" + error.toString() ) ))
+				.then();
 		
 	}
-	   
-    private Mono<Void> processEvent(String topic, Event event) {
+	
+	private Mono<ConsumerRecord<String, Event>> validateTopic(ConsumerRecord<String, Event> consumer ){
+		
+		return Mono.justOrEmpty(consumer.topic())
+				.filter( topic -> topic != null && !topic.isEmpty() )
+				.switchIfEmpty(Mono.error(new ValidationException(Map.of(this.getClass().getSimpleName(), GlobalConstants.EX_VALIDATION_DESC))))
+				.thenReturn(consumer);				
+				
+	}
+	
+	private Mono<ConsumerRecord<String, Event>> validateValue(ConsumerRecord<String, Event> consumer ){
+		
+		return Mono.justOrEmpty(consumer.value())
+				.switchIfEmpty(Mono.error(new ValidationException(Map.of(this.getClass().getSimpleName(), GlobalConstants.EX_VALIDATION_DESC))))
+				.thenReturn(consumer);				
+				
+	}
+	
+    private Mono<Void> processEvent(ConsumerRecord<String, Event> consumer ){
     	
-        return Mono.justOrEmpty(actionsMap.get(topic))
-                   .switchIfEmpty(Mono.defer(() -> 
-                	   handleDefault(event).thenReturn( e -> {})             	   
-                   ))
-                   .flatMap(action -> Mono.fromRunnable(() -> action.accept(event)));
+    	return Mono.justOrEmpty(consumer.topic())
+    	.map(actionsMap::get)
+		.switchIfEmpty(Mono.error(new IllegalArgumentException(this.getClass().getSimpleName() + GlobalConstants.MSG_HANDLER_NONE)))
+    	.flatMap( handle -> handle.apply( consumer.value() ) )
+    	.then();
+    	
     }
-    
-	private Mono<Void> handleDefault(Event event) {
-		  
-		return Mono.fromRunnable( () -> loggerUtil.logInfoAction(this.getClass().getSimpleName(), event.getEventType(), GlobalConstants.MSG_EVENT_ERROR + " : " + event));
-	  
+
+	private <T> Mono<T> logFlowStep(Mono<T> reactiveFlow){
+		
+		return reactiveFlow		
+				.doOnNext( data -> loggerUtil.logInfoAction( this.getClass().getSimpleName(), GlobalConstants.MSG_FLOW_PROCESS, data.toString() ) );	
+			
 	}
 	
 }
