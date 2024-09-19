4c78c38ff165a45e392f7b705d552cb5da8ca4b2
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-09-19 08:17:17 +0200

    feat: add reactive style to some methods

--- ms-position/src/main/java/dev/ime/infrastructure/adapter/CrewMemberRedisProjectorAdapter.java
+++ ms-position/src/main/java/dev/ime/infrastructure/adapter/CrewMemberRedisProjectorAdapter.java
@@ -1,11 +1,15 @@
 package dev.ime.infrastructure.adapter;
 
+import java.util.Map;
+import java.util.Optional;
 import java.util.UUID;
 
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.data.redis.core.ReactiveRedisTemplate;
 import org.springframework.stereotype.Repository;
 
+import dev.ime.application.exception.CreateRedisEntityException;
+import dev.ime.application.exception.EmptyResponseException;
 import dev.ime.config.GlobalConstants;
 import dev.ime.config.LoggerUtil;
 import dev.ime.domain.event.Event;
@@ -15,7 +19,7 @@ import reactor.core.publisher.Mono;
 
 @Repository
 @Qualifier("crewMemberRedisProjectorAdapter")
-public class CrewMemberRedisProjectorAdapter  implements BaseProjectorPort{
+public class CrewMemberRedisProjectorAdapter implements BaseProjectorPort{
 
 	private final LoggerUtil loggerUtil;
     private final ReactiveRedisTemplate<String, CrewMemberRedisEntity> reactiveRedisTemplate;
@@ -26,38 +30,61 @@ public class CrewMemberRedisProjectorAdapter  implements BaseProjectorPort{
 		this.loggerUtil = loggerUtil;
 		this.reactiveRedisTemplate = reactiveRedisTemplate;
 	}
+    
 	@Override
-	public void create(Event event) {
+	public Mono<Void> create(Event event) {
 
-		Mono.justOrEmpty(createEntity(event))
-		.flatMap( entity -> reactiveRedisTemplate.opsForValue().set( generateKey(entity.getCrewMemberId() ), entity))
-		.transform(this::addLogginOptions)
-		.onErrorResume( e -> Mono.fromRunnable( () -> loggerUtil.logSevereAction(e.getMessage())))
-		.subscribe();
+		return Mono.justOrEmpty(event.getEventData())
+				.transform(this::logFlowStep)
+				.flatMap(this::createEntity)		        
+				.transform(this::logFlowStep)
+				.flatMap( entity -> reactiveRedisTemplate.opsForValue().set( generateKey(entity.getCrewMemberId() ), entity))
+				.switchIfEmpty(Mono.error(new EmptyResponseException(Map.of(GlobalConstants.CREWMEMBER_CAT, GlobalConstants.EX_EMPTYRESPONSE_DESC))))
+				.transform(this::addLogginOptions)
+				.then();
 		
 	}
 	
 	@Override
-	public void deleteById(Event event) {
+	public Mono<Void> deleteById(Event event) {
 		
-		Mono.justOrEmpty(event.getEventData().get(GlobalConstants.CREWMEMBER_ID))
-		.switchIfEmpty(Mono.error(new IllegalArgumentException(GlobalConstants.CREWMEMBER_ID + GlobalConstants.MSG_REQUIRED)))
-		.cast(String.class)
-		.map(UUID::fromString)
-		.flatMap( id -> reactiveRedisTemplate.opsForValue().delete( generateKey(id) ))
-		.transform(this::addLogginOptions)
-		.onErrorResume( e -> Mono.fromRunnable( () -> loggerUtil.logSevereAction(e.getMessage()) ))
-		.subscribe();
+		return Mono.justOrEmpty(event.getEventData())
+				.transform(this::logFlowStep)
+				.map( evenData -> evenData.get(GlobalConstants.CREWMEMBER_ID))
+				.cast(String.class)
+				.map(UUID::fromString)			        
+				.transform(this::logFlowStep)
+				.flatMap( id -> reactiveRedisTemplate.opsForValue().delete( generateKey(id) ))	
+				.switchIfEmpty(Mono.error(new EmptyResponseException(Map.of(GlobalConstants.CREWMEMBER_CAT, GlobalConstants.EX_EMPTYRESPONSE_DESC))))
+				.transform(this::addLogginOptions)
+				.then();
 		
 	}
 
-	private CrewMemberRedisEntity createEntity(Event event) {
+	private Mono<CrewMemberRedisEntity> createEntity(Map<String, Object> eventData) {
 		
-		UUID crewMemberId = UUID.fromString( String.valueOf( event.getEventData().get(GlobalConstants.CREWMEMBER_ID) ) );
-		UUID positionId = UUID.fromString( String.valueOf( event.getEventData().get(GlobalConstants.POSITION_ID) ) );
+		return Mono.fromCallable( () -> {
+			
+			UUID crewMemberId = extractUuid(eventData, GlobalConstants.CREWMEMBER_ID);
+			UUID positionId = extractUuid(eventData, GlobalConstants.POSITION_ID);
 
-		return new CrewMemberRedisEntity(crewMemberId, positionId);
+	        if (crewMemberId == null || positionId == null ) {
+	            throw new IllegalArgumentException(GlobalConstants.EX_ILLEGALARGUMENT_DESC);
+	        }
+	        
+			return new CrewMemberRedisEntity(crewMemberId, positionId);
+			
+		}).onErrorMap(e -> new CreateRedisEntityException(Map.of( GlobalConstants.CREWMEMBER_CAT, e.getMessage() )));		
 		
+	}
+
+	private UUID extractUuid(Map<String, Object> eventData, String key) {
+		
+	    return Optional.ofNullable(eventData.get(key))
+	                   .map(Object::toString)
+	                   .map(UUID::fromString)
+	                   .orElse(null);
+	    
 	}
 	
 	private String generateKey(UUID id) {
@@ -67,12 +94,19 @@ public class CrewMemberRedisProjectorAdapter  implements BaseProjectorPort{
 	}
 	
 	private <T> Mono<T> addLogginOptions(Mono<T> reactiveFlow){
-	
+		
+		return reactiveFlow
+				.doOnSuccess( success -> loggerUtil.logInfoAction( this.getClass().getSimpleName(), GlobalConstants.MSG_FLOW_OK, success instanceof Number? GlobalConstants.MSG_MODLINES + success.toString():success.toString() ) )
+		        .doOnError( error -> loggerUtil.logInfoAction( this.getClass().getSimpleName(), GlobalConstants.MSG_FLOW_ERROR, error.toString() ) )
+		        .doFinally( signal -> loggerUtil.logInfoAction( this.getClass().getSimpleName(), GlobalConstants.MSG_FLOW_RESULT, signal.toString() ) );		
+			
+	}	
+
+	private <T> Mono<T> logFlowStep(Mono<T> reactiveFlow){
+		
 		return reactiveFlow		
-				.doOnSuccess(o -> loggerUtil.logInfoAction(this.getClass().getSimpleName(), GlobalConstants.MSG_FLOW_OK,  o.toString()))
-		        .doOnError(e -> loggerUtil.logInfoAction(this.getClass().getSimpleName(), GlobalConstants.MSG_FLOW_ERROR, e.toString()))
-		        .doFinally(signalType -> loggerUtil.logInfoAction(this.getClass().getSimpleName(), GlobalConstants.MSG_FLOW_RESULT, signalType.toString()));		
-	
+				.doOnNext( data -> loggerUtil.logInfoAction( this.getClass().getSimpleName(), GlobalConstants.MSG_FLOW_PROCESS, data.toString() ) );	
+			
 	}
 	
 }
