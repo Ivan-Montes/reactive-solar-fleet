18c63372d6bfb7a175d812efb6dd6a3d5cfeaa35
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-09-19 08:14:21 +0200

    feat: add reactive return type

--- ms-position/src/main/java/dev/ime/domain/port/inbound/SubscriberPort.java
+++ ms-position/src/main/java/dev/ime/domain/port/inbound/SubscriberPort.java
@@ -3,9 +3,10 @@ package dev.ime.domain.port.inbound;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 
 import dev.ime.domain.event.Event;
+import reactor.core.publisher.Mono;
 
 public interface SubscriberPort {
 
-	void onMessage(ConsumerRecord<String, Event> consumerRecord);
+	Mono<Void> onMessage(ConsumerRecord<String, Event> consumerRecord);
 
 }
