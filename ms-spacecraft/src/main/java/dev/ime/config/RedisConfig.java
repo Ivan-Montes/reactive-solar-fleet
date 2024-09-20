fa4f5dcc90d3dbfa22e72eb766df0259bb9920d5
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-09-20 07:36:32 +0200

    feat: add Redis config

---
+++ ms-spacecraft/src/main/java/dev/ime/config/RedisConfig.java
@@ -0,0 +1,47 @@
+package dev.ime.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+import org.springframework.data.redis.serializer.Jackson2JsonRedisSerializer;
+import org.springframework.data.redis.serializer.RedisSerializationContext;
+import org.springframework.data.redis.serializer.RedisSerializer;
+
+import dev.ime.infrastructure.entity.CrewMemberRedisEntity;
+import dev.ime.infrastructure.entity.ShipclassRedisEntity;
+
+@Configuration
+public class RedisConfig {
+
+    @Bean
+    ReactiveRedisTemplate<String, ShipclassRedisEntity> shipclassReactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+        Jackson2JsonRedisSerializer<ShipclassRedisEntity> serializer = new Jackson2JsonRedisSerializer<>(ShipclassRedisEntity.class);
+        
+        RedisSerializationContext.RedisSerializationContextBuilder<String, ShipclassRedisEntity> builder =
+            RedisSerializationContext.newSerializationContext( RedisSerializer.string() );
+        
+        RedisSerializationContext<String, ShipclassRedisEntity> context = builder
+            .value(serializer)
+            .build();
+        
+        return new ReactiveRedisTemplate<>(factory, context);
+        
+    }
+
+    @Bean
+    ReactiveRedisTemplate<String, CrewMemberRedisEntity> crewMemberReactiveRedisTemplate(ReactiveRedisConnectionFactory factory) {
+        Jackson2JsonRedisSerializer<CrewMemberRedisEntity> serializer = new Jackson2JsonRedisSerializer<>(CrewMemberRedisEntity.class);
+        
+        RedisSerializationContext.RedisSerializationContextBuilder<String, CrewMemberRedisEntity> builder =
+            RedisSerializationContext.newSerializationContext( RedisSerializer.string() );
+        
+        RedisSerializationContext<String, CrewMemberRedisEntity> context = builder
+            .value(serializer)
+            .build();
+        
+        return new ReactiveRedisTemplate<>(factory, context);
+        
+    }
+    
+}
