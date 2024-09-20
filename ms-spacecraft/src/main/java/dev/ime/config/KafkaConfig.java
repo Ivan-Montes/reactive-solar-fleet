177533a062dce4208a8efd8fedb77a07953c81f0
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-09-20 07:38:13 +0200

    feat: add Kafka config

---
+++ ms-spacecraft/src/main/java/dev/ime/config/KafkaConfig.java
@@ -0,0 +1,23 @@
+package dev.ime.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.config.TopicBuilder;
+import org.springframework.kafka.core.KafkaAdmin;
+import org.springframework.kafka.core.KafkaAdmin.NewTopics;
+
+@Configuration
+public class KafkaConfig {
+
+	@Bean
+	KafkaAdmin.NewTopics topics() {
+		
+		return new NewTopics(
+				TopicBuilder.name(GlobalConstants.SPACECRAFT_CREATED).build(),
+				TopicBuilder.name(GlobalConstants.SPACECRAFT_UPDATED).build(),
+				TopicBuilder.name(GlobalConstants.SPACECRAFT_DELETED).build()
+				);
+		
+	}
+	
+}
