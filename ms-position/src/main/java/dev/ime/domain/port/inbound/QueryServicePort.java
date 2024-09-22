dd905a122f6eeffc3e0d5682eefe12eb44ef6a94
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-09-22 09:33:46 +0200

    feat: add pageable parameter to find all entities

--- ms-position/src/main/java/dev/ime/domain/port/inbound/QueryServicePort.java
+++ ms-position/src/main/java/dev/ime/domain/port/inbound/QueryServicePort.java
@@ -2,12 +2,14 @@ package dev.ime.domain.port.inbound;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Pageable;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface QueryServicePort<T> {
 
-	Flux<T>getAll();
+	Flux<T>getAll(Pageable pageable);
 	Mono<T>getById(UUID id);
 	
 }
--- ms-position/src/main/java/dev/ime/domain/port/outbound/ReadRepositoryPort.java
+++ ms-position/src/main/java/dev/ime/domain/port/outbound/ReadRepositoryPort.java
@@ -2,12 +2,14 @@ package dev.ime.domain.port.outbound;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Pageable;
+
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 public interface ReadRepositoryPort<T> {
 
-	Flux<T> findAll();
+	Flux<T> findAll(Pageable pageable);
 	Mono<T> findById(UUID id);
 	Mono<T> findByName(String name);
 	
