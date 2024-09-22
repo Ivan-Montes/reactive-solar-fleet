1f39969e66f03165cf3f1e1fd2032feb508114c1
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-09-22 09:40:52 +0200

    refactor: replace domain class for dto

--- ms-position/src/main/java/dev/ime/application/service/QueryService.java
+++ ms-position/src/main/java/dev/ime/application/service/QueryService.java
@@ -2,47 +2,51 @@ package dev.ime.application.service;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import dev.ime.application.dispatcher.QueryDispatcher;
+import dev.ime.application.dto.PositionDto;
 import dev.ime.application.usecase.GetAllPositionQuery;
 import dev.ime.application.usecase.GetByIdPositionQuery;
+import dev.ime.config.PositionMapper;
 import dev.ime.domain.model.Position;
 import dev.ime.domain.port.inbound.QueryServicePort;
-import dev.ime.domain.query.Query;
 import dev.ime.domain.query.QueryHandler;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
-public class QueryService implements QueryServicePort<Position> {
+public class QueryService implements QueryServicePort<PositionDto> {
 
 	private final QueryDispatcher queryDispatcher;
-	
-	public QueryService(QueryDispatcher queryDispatcher) {
+	private final PositionMapper mapper;	
+
+	public QueryService(QueryDispatcher queryDispatcher, PositionMapper mapper) {
 		super();
 		this.queryDispatcher = queryDispatcher;
+		this.mapper = mapper;
 	}
 
 	@Override
-	public Flux<Position> getAll() {
+	public Flux<PositionDto> getAll(Pageable pageable) {
 		
-		return handleQuery(new GetAllPositionQuery());
+		GetAllPositionQuery query = new GetAllPositionQuery(pageable);
+		QueryHandler<Flux<Position>> handler = queryDispatcher.getQueryHandler(query);		
+		Flux<Position> flow = handler.handle(query);
+		
+		return flow.map(mapper::fromDomainToDto);
 		
 	}
 
 	@Override
-	public Mono<Position> getById(UUID id) {		
-
-		return handleQuery(new GetByIdPositionQuery(id));
+	public Mono<PositionDto> getById(UUID id) {		
 		
-	}
-	
-	private <T> T handleQuery(Query query) {
+		GetByIdPositionQuery query = new GetByIdPositionQuery(id);
+		QueryHandler<Mono<Position>> handler = queryDispatcher.getQueryHandler(query);		
+		Mono<Position> flow = handler.handle(query);
 		
-		QueryHandler<T> handler = queryDispatcher.getQueryHandler(query);
-		
-		return handler.handle(query);
+		return flow.map(mapper::fromDomainToDto);
 		
 	}
 	
