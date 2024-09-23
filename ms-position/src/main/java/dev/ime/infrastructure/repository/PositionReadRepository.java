5b485de96277d508e9646023fdc69ec9a25bd49f
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-09-23 07:34:39 +0200

    feat: add sorting repository

--- ms-position/src/main/java/dev/ime/infrastructure/repository/PositionReadRepository.java
+++ ms-position/src/main/java/dev/ime/infrastructure/repository/PositionReadRepository.java
@@ -2,13 +2,17 @@ package dev.ime.infrastructure.repository;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.repository.reactive.ReactiveSortingRepository;
 
 import dev.ime.infrastructure.entity.PositionJpaEntity;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-public interface PositionReadRepository extends ReactiveCrudRepository<PositionJpaEntity, UUID>{
+public interface PositionReadRepository extends ReactiveCrudRepository<PositionJpaEntity, UUID>, ReactiveSortingRepository<PositionJpaEntity, UUID>{
 
 	Mono<PositionJpaEntity> findByPositionName(String name);
-	
+    Flux<PositionJpaEntity> findAllBy(Pageable pageable);
+
 }
