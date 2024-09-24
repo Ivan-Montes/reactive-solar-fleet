2cb521ba6eee4015f1c3573faf0a71f471509f6a
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-09-24 07:47:36 +0200

    feat: add mapper to transform class before return back

--- ms-shipclass/src/main/java/dev/ime/application/service/QueryService.java
+++ ms-shipclass/src/main/java/dev/ime/application/service/QueryService.java
@@ -2,47 +2,51 @@ package dev.ime.application.service;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
 import dev.ime.application.dispatcher.QueryDispatcher;
+import dev.ime.application.dto.ShipclassDto;
 import dev.ime.application.usecase.GetAllShipclassQuery;
 import dev.ime.application.usecase.GetByIdShipclassQuery;
+import dev.ime.config.ShipclassMapper;
 import dev.ime.domain.model.Shipclass;
 import dev.ime.domain.port.inbound.QueryServicePort;
-import dev.ime.domain.query.Query;
 import dev.ime.domain.query.QueryHandler;
 import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @Service
-public class QueryService implements QueryServicePort<Shipclass> {
+public class QueryService implements QueryServicePort<ShipclassDto> {
 
 	private final QueryDispatcher queryDispatcher;
+	private final ShipclassMapper mapper;	
 	
-	public QueryService(QueryDispatcher queryDispatcher) {
+	public QueryService(QueryDispatcher queryDispatcher, ShipclassMapper mapper) {
 		super();
 		this.queryDispatcher = queryDispatcher;
+		this.mapper = mapper;
 	}
 
 	@Override
-	public Flux<Shipclass> getAll() {
+	public Flux<ShipclassDto> getAll(Pageable pageable) {
+
+		GetAllShipclassQuery query = new GetAllShipclassQuery(pageable);
+		QueryHandler<Flux<Shipclass>> handler = queryDispatcher.getQueryHandler(query);		
+		Flux<Shipclass> flow = handler.handle(query);
 		
-		return handleQuery(new GetAllShipclassQuery());
+		return flow.map(mapper::fromDomainToDto);
 		
 	}
 
 	@Override
-	public Mono<Shipclass> getById(UUID id) {		
+	public Mono<ShipclassDto> getById(UUID id) {		
 
-		return handleQuery(new GetByIdShipclassQuery(id));
+		GetByIdShipclassQuery query = new GetByIdShipclassQuery(id);
+		QueryHandler<Mono<Shipclass>> handler = queryDispatcher.getQueryHandler(query);		
+		Mono<Shipclass> flow = handler.handle(query);
 		
-	}
-	
-	private <T> T handleQuery(Query query) {
-		
-		QueryHandler<T> handler = queryDispatcher.getQueryHandler(query);
-		
-		return handler.handle(query);
+		return flow.map(mapper::fromDomainToDto);
 		
 	}
 	
