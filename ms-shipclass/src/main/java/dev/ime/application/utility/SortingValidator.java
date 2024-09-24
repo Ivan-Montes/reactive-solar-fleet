7e0404afe19fe95f7387ba25c8f8d02d5d84630b
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-09-24 07:45:33 +0200

    feat: add validator for pagination options

---
+++ ms-shipclass/src/main/java/dev/ime/application/utility/SortingValidator.java
@@ -0,0 +1,78 @@
+package dev.ime.application.utility;
+
+import java.util.Map;
+import java.util.Set;
+
+import org.springframework.stereotype.Component;
+
+import dev.ime.application.dto.ShipclassDto;
+import dev.ime.domain.model.Shipclass;
+
+@Component
+public class SortingValidator {
+
+	private final Map<Class<?>, Set<String>> validSortFieldsMap;
+	private final ReflectionUtils reflectionUtils;
+	
+	public SortingValidator(ReflectionUtils reflectionUtils) {
+		super();
+		this.reflectionUtils = reflectionUtils;
+		this.validSortFieldsMap = initializeMap();
+	}
+	
+	private Map<Class<?>, Set<String>> initializeMap() {
+		
+		return Map.of(
+				ShipclassDto.class, reflectionUtils.getFieldNames(Shipclass.class)
+				);		
+	}
+	
+    private boolean isValidKeyclass(Class<?> keyClass) {
+    	
+        return validSortFieldsMap
+        		.containsKey(keyClass);
+        
+    }
+
+    public String getDefaultSortField(Class<?> keyClass) {
+    	
+    	Set<String> validFieldsSet =  validSortFieldsMap
+        		.get(keyClass);
+    	
+        if (validFieldsSet == null || validFieldsSet.isEmpty()) {
+            return "";
+        }
+        
+        String expectedIdField = getExpectedIdField(keyClass);
+
+    	return validFieldsSet
+        		.stream()
+        		.filter( fieldName -> fieldName.toLowerCase().equals(expectedIdField))
+        		.findFirst()
+        		.orElse(validFieldsSet.iterator().next());        		
+        		
+    }
+    
+    private String getExpectedIdField(Class<?> keyClass) {
+    	
+    	String sufixDto = keyClass.getSimpleName().toLowerCase();
+    	String sufixClass = sufixDto.substring(0, sufixDto.length() - 3);
+    	
+        return sufixClass + "id";
+
+    }
+    
+    public boolean isValidSortField(Class<?> keyClass, String sortField) {
+    	
+    	if ( !isValidKeyclass(keyClass) ) {
+    		return false;
+    	}
+    	
+    	return  validSortFieldsMap
+        		.get(keyClass)
+        		.stream()
+        		.anyMatch( fieldName -> fieldName.equals(sortField));    
+        
+    }
+    
+}
