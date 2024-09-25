3576c441c5431be91aada1deaa12a9d70a3c2801
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-09-25 07:40:51 +0200

    feat: add method and interface for sorting

--- ms-shipclass/src/main/java/dev/ime/infrastructure/repository/ShipclassReadRepository.java
+++ ms-shipclass/src/main/java/dev/ime/infrastructure/repository/ShipclassReadRepository.java
@@ -2,13 +2,17 @@ package dev.ime.infrastructure.repository;
 
 import java.util.UUID;
 
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.repository.reactive.ReactiveCrudRepository;
+import org.springframework.data.repository.reactive.ReactiveSortingRepository;
 
 import dev.ime.infrastructure.entity.ShipclassJpaEntity;
+import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
-public interface ShipclassReadRepository extends ReactiveCrudRepository<ShipclassJpaEntity, UUID>{
+public interface ShipclassReadRepository extends ReactiveCrudRepository<ShipclassJpaEntity, UUID>, ReactiveSortingRepository<ShipclassJpaEntity, UUID>{
 
 	Mono<ShipclassJpaEntity> findByShipclassName(String name);
+	Flux<ShipclassJpaEntity> findAllBy(Pageable pageable);
 	
 }
