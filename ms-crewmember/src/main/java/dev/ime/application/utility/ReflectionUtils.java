57c7fe9285831ad576f67636a0bd418eb3c777b1
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-10-10 07:31:01 +0200

    feat: add utils

---
+++ ms-crewmember/src/main/java/dev/ime/application/utility/ReflectionUtils.java
@@ -0,0 +1,24 @@
+package dev.ime.application.utility;
+
+import java.lang.reflect.Field;
+import java.util.Arrays;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class ReflectionUtils {	
+	
+	public ReflectionUtils() {
+		super();
+	}
+
+	public Set<String> getFieldNames(Class<?> clazz) {
+        return Arrays.stream(clazz.getDeclaredFields())
+                     .map(Field::getName)
+                     .collect(Collectors.toSet());
+        
+    }
+	
+}
