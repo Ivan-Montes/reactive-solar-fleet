f9afbb7b13050baa3c24d69b539b2544b95877e3
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-10-30 07:40:14 +0100

    feat: add R2dbc config

---
+++ ms-crewmember/src/test/java/dev/ime/config/R2dbcConfigTest.java
@@ -0,0 +1,41 @@
+package dev.ime.config;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.r2dbc.core.R2dbcEntityTemplate;
+
+@ExtendWith(MockitoExtension.class)
+class R2dbcConfigTest {
+
+	@Mock
+	private R2dbcConfigProperties r2dbcConfigProperties;
+
+	@InjectMocks
+	private R2dbcConfig r2dbcConfig;
+	
+	@Test
+	void r2dbcEntityTemplate_WithMock_ReturnR2dbcEntityTemplate() {
+		
+		Mockito.when(r2dbcConfigProperties.getDriver()).thenReturn("postgresql");
+    	Mockito.when(r2dbcConfigProperties.getHost()).thenReturn("localhost");
+    	Mockito.when(r2dbcConfigProperties.getPort()).thenReturn("5432");
+    	Mockito.when(r2dbcConfigProperties.getUsername()).thenReturn("user");
+        Mockito.when(r2dbcConfigProperties.getPassword()).thenReturn("password");
+        Mockito.when(r2dbcConfigProperties.getDatabase()).thenReturn("testdb");
+        
+        R2dbcEntityTemplate r2dbcEntityTemplate = r2dbcConfig.r2dbcEntityTemplate(r2dbcConfigProperties);
+        
+        org.junit.jupiter.api.Assertions.assertAll( 
+				()-> Assertions.assertThat(r2dbcEntityTemplate).isNotNull(),
+				()-> Assertions.assertThat(r2dbcEntityTemplate.getDatabaseClient()).isNotNull(),
+				()-> Assertions.assertThat(r2dbcEntityTemplate.getDatabaseClient().getConnectionFactory()).isNotNull()
+				);
+	}
+
+}
