00529f4962c5c46cd2af3beaaa210c345ef1cd8f
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-11-07 07:36:53 +0100

    test: add for kafka subscribers

---
+++ ms-crewmember/src/test/java/dev/ime/infrastructure/adapter/KafkaPositionSubscriberAdapterTest.java
@@ -0,0 +1,112 @@
+package dev.ime.infrastructure.adapter;
+
+
+import java.time.Instant;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.UUID;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import dev.ime.config.GlobalConstants;
+import dev.ime.config.LoggerUtil;
+import dev.ime.domain.event.Event;
+import dev.ime.domain.port.outbound.BaseProjectorPort;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+@ExtendWith(MockitoExtension.class)
+class KafkaPositionSubscriberAdapterTest {
+
+	@Mock
+	private LoggerUtil loggerUtil;
+	
+	@Mock
+    private BaseProjectorPort baseProjectorPort;
+
+	@InjectMocks
+	private KafkaPositionSubscriberAdapter kafkaPositionSubscriberAdapter;	
+
+	private ConsumerRecord<String, Event> consumerRecord;
+	private Event event;
+	private final UUID eventId = UUID.randomUUID();
+	private final String eventCategory = GlobalConstants.POSITION_CAT;
+	private final String eventType = GlobalConstants.POSITION_DELETED;
+	private final Instant eventTimestamp = Instant.now();
+	private final Map<String, Object> eventData = new HashMap<>();
+
+	@BeforeEach
+	private void setUp() {
+		
+		event = new Event(
+				eventId,
+				eventCategory,
+				eventType,
+				eventTimestamp,
+				eventData);
+		
+	}
+	
+	@Test
+	void onMessage_WithConsumerRecord_ProcessOk() {
+		
+		consumerRecord = createConsumerRecord(eventType);
+		Mockito.when(baseProjectorPort.deleteById(Mockito.any(Event.class))).thenReturn(Mono.empty());
+		
+		StepVerifier
+		.create(kafkaPositionSubscriberAdapter.onMessage(consumerRecord))
+		.verifyComplete();
+		
+		Mockito.verify(baseProjectorPort).deleteById(Mockito.any(Event.class));
+
+	}
+
+	@Test
+	void onMessage_WithBadTopic_GenerateError() {
+		
+		consumerRecord = createConsumerRecord("");
+		
+		StepVerifier
+		.create(kafkaPositionSubscriberAdapter.onMessage(consumerRecord))
+		.verifyComplete();
+		
+		Mockito.verify(loggerUtil).logSevereAction(Mockito.anyString());
+		
+	}
+
+	@Test
+	void onMessage_ThrowEx_ManageError() {
+		
+		consumerRecord = createConsumerRecord(eventType);
+	    RuntimeException testException = new RuntimeException(GlobalConstants.MSG_UNKNOWDATA);
+	    Mockito.doThrow(testException).when(baseProjectorPort).deleteById(Mockito.any(Event.class));
+	    
+	    StepVerifier
+		.create(kafkaPositionSubscriberAdapter.onMessage(consumerRecord))
+		.verifyComplete();		
+		
+		Mockito.verify(loggerUtil).logSevereAction(Mockito.anyString());
+		Mockito.verify(baseProjectorPort).deleteById(Mockito.any(Event.class));
+
+	}
+	
+	private ConsumerRecord<String, Event> createConsumerRecord(String eventType) {
+		
+		return new ConsumerRecord<>(
+				eventType,
+				1,
+				1L,
+				"",
+				event
+				);
+		
+	}
+	
+}
