0fa26eca60da7a51cc33b9bb919d371ad9f83ada
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-11-09 07:42:00 +0100

    test: add for Redis config

---
+++ ms-position/src/test/java/dev/ime/config/RedisConfigTest.java
@@ -0,0 +1,35 @@
+package dev.ime.config;
+
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.data.redis.connection.ReactiveRedisConnectionFactory;
+import org.springframework.data.redis.core.ReactiveRedisTemplate;
+
+import dev.ime.infrastructure.entity.CrewMemberRedisEntity;
+
+@ExtendWith(MockitoExtension.class)
+class RedisConfigTest {
+
+    @Mock
+    private ReactiveRedisConnectionFactory redisFactory;
+
+    @InjectMocks
+    private RedisConfig redisConfig;
+
+	@Test
+	void reactiveRedisTemplate_ShouldCreateCorrectTemplate() {
+		
+        ReactiveRedisTemplate<String, CrewMemberRedisEntity> template = redisConfig.reactiveRedisTemplate(redisFactory);
+
+        org.junit.jupiter.api.Assertions.assertAll(
+        		()-> Assertions.assertThat(template).isNotNull()
+        		);
+
+	}
+
+}
