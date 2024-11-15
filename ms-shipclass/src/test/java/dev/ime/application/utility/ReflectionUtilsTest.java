fd03964e187b902db83a21ebacbde767d300825e
IvanM <87393011+Ivan-Montes@users.noreply.github.com>
2024-11-15 07:45:58 +0100

    test: add for reflection utils

---
+++ ms-shipclass/src/test/java/dev/ime/application/utility/ReflectionUtilsTest.java
@@ -0,0 +1,30 @@
+package dev.ime.application.utility;
+
+
+import java.util.Set;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import dev.ime.domain.model.Shipclass;
+
+@ExtendWith(MockitoExtension.class)
+class ReflectionUtilsTest {
+
+	@InjectMocks
+	private ReflectionUtils reflectionUtils;
+	
+	@Test
+	void getFieldNames_WithDomainClass_ReturnFieldSet() {
+		Set<String> fieldNamesSet =  reflectionUtils.getFieldNames(Shipclass.class);
+		
+		org.junit.jupiter.api.Assertions.assertAll(
+				()-> Assertions.assertThat(fieldNamesSet).isNotNull(),
+				()-> Assertions.assertThat(fieldNamesSet).hasSize(3)
+				);
+	}
+
+}
